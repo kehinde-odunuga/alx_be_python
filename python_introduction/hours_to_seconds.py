3087b44c5d13be7a8a2d1f50226be1a54523c761
kehinde-odunuga <omobolaji92@gmail.com>
2024-09-14 14:14:51 +0100

    3. Convert Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+# Write a Python script that converts a specific number of hours into seconds.
+
+hours = 2
+minutes = 60 * hours
+seconds = 60 * minutes
+
+print(f"{hours} hour(s) is {seconds} seconds.")
