96a50d42f4152ee7ea3e4bf71eb698c15932a31d
kehinde-odunuga <omobolaji92@gmail.com>
2024-09-23 23:54:04 +0100

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,8 +9,8 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             result = num1 * num2
         case "divide":
-            if num2 == 0:
-                return "Cannot divide by zero."
-            else:
-                result = num1 / num2    
+            if num2 != 0:
+                result = num1 / num2 
+            elif num2 == 0:
+                return "Cannot divide by zero."    
     return result
